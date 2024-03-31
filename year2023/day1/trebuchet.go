465ead511950adfeab8affcf8f92b635107d9861
phaymanWW <paul.hayman@ww.com>
2024-03-31 15:58:58 -0400

    working test and decoder

--- year2023/day1/trebuchet.go
+++ year2023/day1/trebuchet.go
@@ -1,7 +1,35 @@
 package day1
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
 
 func Run() { 
 	fmt.Println("day1")
+}
+
+func DecodeLine(line string) int {
+	var r1, r2 string
+	for start, end := 0, len(line) - 1; start <= end; start, end = start + 1, end - 1 {
+		if r1 == "" { 
+			if _, err := strconv.Atoi(string(line[start])); err == nil { 
+				r1 = string(line[start])
+			}
+		}
+		if r2 == "" {
+			if _, err := strconv.Atoi(string(line[end])); err == nil { 
+				r2 = string(line[end])
+			}
+		}
+		if r1 != "" && r2 != ""{
+			break
+		}
+	}
+	result, err := strconv.Atoi(r1 + "" + r2)
+	if err != nil { 
+		panic(fmt.Sprintf("%v is not a number", result))
+	}
+	return result
 }
--- year2023/day1/trebuchet_test.go
+++ year2023/day1/trebuchet_test.go
@@ -2,18 +2,20 @@ package day1
 
 import "testing"
 
-func TestRun(t *testing.T){
-	// cases := []struct {
-    //     in, want string
-    // }{
-    //     {"Hello, world", "dlrow ,olleH"},
-    //     {"Hello, 世界", "界世 ,olleH"},
-    //     {"", ""},
-    // }
-    // for _, c := range cases {
-    //     got := Run()
-    //     if got != c.want {
-    //         t.Errorf("ReverseRunes(%q) == %q, want %q", c.in, got, c.want)
-    //     }
-    // }
+func TestDecodeLine(t *testing.T){
+	cases := []struct {
+        in string
+		want int
+    }{
+        {"1abc2", 12},
+        {"pqr3stu8vwx", 38},
+        {"a1b2c3d4e5f", 15},
+        {"treb7uchet", 7},
+    }
+    for _, c := range cases {
+        got := DecodeLine(c.in)
+        if got != c.want {
+            t.Errorf("DecodeLine(%q) == %v, want %v", c.in, got, c.want)
+        }
+    }
 }
