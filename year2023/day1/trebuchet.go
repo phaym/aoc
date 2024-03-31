f64bf765f66a2afeff479e815a6273550555d9c3
phaymanWW <paul.hayman@ww.com>
2024-03-31 17:42:45 -0400

    refactor and test file decoder

--- year2023/day1/trebuchet.go
+++ year2023/day1/trebuchet.go
@@ -15,38 +15,41 @@ func Run() {
 		panic(err)
 	}
 	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	total := 0
-	for scanner.Scan() {
-		total += DecodeLine(scanner.Text())
-	}
+	total := DecodeFile(file)
 	fmt.Println(total)
 }
 
-var tokenMap = map[string]int{
-	"1":     1,
-	"2":     2,
-	"3":     3,
-	"4":     4,
-	"5":     5,
-	"6":     6,
-	"7":     7,
-	"8":     8,
-	"9":     9,
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
+func DecodeFile(file *os.File) (total int) {
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		total += DecodeLine(scanner.Text())
+	}
+	return
 }
 
+
 func DecodeLine(line string) int {
+	tokenMap := map[string]int{
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
 	var first, second int
 	lowest, highest := len(line), -1
 	for k, v := range tokenMap {
--- year2023/day1/trebuchet_test.go
+++ year2023/day1/trebuchet_test.go
@@ -1,6 +1,9 @@
 package day1
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestDecodeLine(t *testing.T) {
 	cases := []struct {
@@ -26,6 +29,27 @@ func TestDecodeLine(t *testing.T) {
 		got := DecodeLine(c.in)
 		if got != c.want {
 			t.Errorf("DecodeLine(%q) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
+
+
+func TestDecodeFile(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{ "input.test.txt", 281 },
+	}
+	for _, c := range cases {
+		file, err := os.Open(c.in)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+		got := DecodeFile(file)
+		if got != c.want {
+			t.Errorf("DecodeFile(%q) == %v, want %v", c.in, got, c.want)
 		}
 	}
 }
