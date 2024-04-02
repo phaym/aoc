14a3b6b4c4813417de43922f4f1d4eaedcbaa5fd
phaymanWW <paul.hayman@ww.com>
2024-04-01 22:24:26 -0400

    calculate card

--- year2023/day4/card.go
+++ year2023/day4/card.go
@@ -3,6 +3,7 @@ package day4
 import (
 	"aoc/util"
 	"fmt"
+	"strings"
 )
 
 func Run() {
@@ -11,10 +12,52 @@ func Run() {
 }
 
 func A(path string) (total int) {
-
 	lines := util.ReadLinesFromFile(path)
+	points := make(chan int)
 	for line := range lines {
-		fmt.Println(line)
+		card := CalcPoints(line)
+		points <- card
+	}
+	for point := range points {
+		total += point
+		fmt.Println(point)
 	}
 	return total
+}
+
+type Card struct {
+	id      int
+	winners map[string]int
+	played  map[string]int
+}
+
+func NewCard() *Card {
+	return &Card{winners: make(map[string]int), played: make(map[string]int)}
+}
+
+func CalcPoints(line string) (points int) {
+	card := ParseCard(line)
+	for k, _ := range card.played {
+		if _, ok := card.winners[k]; ok {
+			if points == 0 {
+				points = 1
+			} else {
+				points *= 2
+			}
+		}
+	}
+	return
+}
+
+func ParseCard(line string) *Card {
+	card := NewCard()
+	split := strings.Split(line, ":")
+	game := strings.Split(split[1], "|")
+	for _, winner := range strings.Split(strings.TrimSpace(game[0]), " ") {
+		card.winners[strings.TrimSpace(winner)] = 1
+	}
+	for _, played := range strings.Split(strings.TrimSpace(game[1]), " ") {
+		card.played[strings.TrimSpace(played)] = 1
+	}
+	return card
 }
--- year2023/day4/card_test.go
+++ year2023/day4/card_test.go
@@ -18,3 +18,18 @@ func TestA(t *testing.T) {
 		}
 	}
 }
+
+func TestCalcPoints(t *testing.T) {
+	cases := []struct {
+		in   string
+		want int
+	}{
+		{"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8},
+	}
+	for _, c := range cases {
+		got := CalcPoints(c.in)
+		if got != c.want {
+			t.Errorf("CalcPoints(%q) == %v, want %v", c.in, got, c.want)
+		}
+	}
+}
