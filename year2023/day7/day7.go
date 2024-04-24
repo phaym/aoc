35ac73261bbd4115eeaa3a51ee1c68b3545e3f82
phaymanWW <paul.hayman@ww.com>
2024-04-23 21:37:48 -0400

    assign ranks

--- year2023/day7/day7.go
+++ year2023/day7/day7.go
@@ -3,6 +3,7 @@ package day7
 import (
 	"aoc/util/file"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -13,12 +14,23 @@ func Run() {
 }
 
 type Card struct {
-	hand string
-	bid  int
+	hand       string
+	bid        int
+	typeCounts map[string]int
+	rank       int
 }
 
-func A(path string) int {
+const (
+	DEFAULT = iota
+	HIGH
+	TWO
+	THREE
+	FULL
+	FOUR
+	FIVE
+)
 
+func A(path string) int {
 	lines := file.ReadLinesFromFile(path)
 	cards := make([]Card, 0)
 	for line := range lines {
@@ -29,7 +41,38 @@ func A(path string) int {
 }
 
 func totalWinnings(cards []Card) int {
+	for i := range cards {
+		calcHand(&cards[i])
+	}
+
 	return 3
+}
+
+func calcHand(card *Card) {
+	for i := 0; i < len(card.hand); i++ {
+		card.typeCounts[string(card.hand[i])]++
+	}
+	card.rank = getRank(card)
+}
+
+func getRank(card *Card) int {
+	counts := make([]int, 0)
+	for _, val := range card.typeCounts {
+		counts = append(counts, val)
+	}
+	if slices.Contains(counts, 5) {
+		return FIVE
+	} else if slices.Contains(counts, 4) {
+		return FOUR
+	} else if slices.Contains(counts, 3) && slices.Contains(counts, 2) {
+		return FULL
+	} else if slices.Contains(counts, 3) {
+		return THREE
+	} else if slices.Contains(counts, 2) {
+		return TWO
+	} else {
+		return HIGH
+	}
 }
 
 func parseLine(line string) Card {
@@ -37,7 +80,9 @@ func parseLine(line string) Card {
 	hand := split[0]
 	bid, _ := strconv.Atoi(split[1])
 	return Card{
-		hand,
-		bid,
+		hand:       hand,
+		bid:        bid,
+		typeCounts: make(map[string]int),
+		rank:       DEFAULT,
 	}
 }
