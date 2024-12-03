e06d9a96e72d9d9b9ff9016905034b2c5fca3cae
phaym <paul.haym@gmail.com>
2024-12-03 00:35:00 -0500

    refactor again

--- year2024/src/day1.rs
+++ year2024/src/day1.rs
@@ -1,81 +1,82 @@
 use std::fs;
 
-struct IdLists {
+struct Ids {
     left: Vec<i32>,
     right: Vec<i32>,
 }
 
-impl IdLists {
+impl Ids {
     pub fn new() -> Self {
-        IdLists {
+        Ids {
             left: Vec::new(),
             right: Vec::new(),
         }
     }
-
-    pub fn parse_line(&mut self, line: &str) {
-        let ids: Vec<i32> = line
-            .split_whitespace()
-            .map(|id| id.parse::<i32>().expect("invalid formatting for line"))
-            .collect();
-        self.left.push(ids[0]);
-        self.right.push(ids[1]);
-    }
 }
 
-fn read_file(file_path: &str) -> IdLists {
+fn parse_line(line: &str) -> (i32, i32) {
+    let ids: Vec<i32> = line
+        .split_whitespace()
+        .map(|id| id.parse::<i32>().expect("invalid formatting for line"))
+        .collect();
+    (ids[0], ids[1])
+}
+
+fn parse_file(file_path: &str) -> Ids {
     let file_contents = fs::read_to_string(file_path).expect("invalid input file");
-    let mut id_lists = IdLists::new();
+    let mut ids = Ids::new();
     for line in file_contents.lines() {
-        id_lists.parse_line(line);
+        let (left, right) = parse_line(line);
+        ids.left.push(left);
+        ids.right.push(right);
     }
-    id_lists
+    ids
 }
 
 pub mod part1 {
-    use super::{read_file, IdLists};
+    use super::{parse_file, Ids};
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day1 - part1");
-        let IdLists { left, right } = read_file(file_path);
-        let distance = calculate_distance(left, right);
+        let ids = parse_file(file_path);
+        let distance = calculate_distance(ids);
         println!("distance: {}", distance);
         distance
     }
 
-    fn calculate_distance(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
-        left.sort();
-        right.sort();
+    fn calculate_distance(mut ids: Ids) -> i32 {
+        ids.left.sort();
+        ids.right.sort();
         let mut distance = 0;
-        for i in 0..left.len() {
-            distance += (left[i] - right[i]).abs();
+        for i in 0..ids.left.len() {
+            distance += (ids.left[i] - ids.right[i]).abs();
         }
         distance
     }
 }
 
 pub mod part2 {
-    use super::{read_file, IdLists};
+    use super::{parse_file, Ids};
     use std::collections::HashMap;
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day1 - part2");
-        let IdLists { left, right } = read_file(file_path);
-        let similarity_score = calculate_similarity_score(left, right);
+        let ids = parse_file(file_path);
+        let similarity_score = calculate_similarity_score(ids);
         println!("similarity_score: {}", similarity_score);
         similarity_score
     }
 
-    fn calculate_similarity_score(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    fn calculate_similarity_score(ids: Ids) -> i32 {
         let mut similarity_score = 0;
         let mut right_counts: HashMap<i32, i32> = HashMap::new();
-        for right in right {
+        for right in ids.right {
             right_counts
                 .entry(right)
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
         }
-        for left in left {
+        for left in ids.left {
             if let Some(right) = right_counts.get(&left) {
                 similarity_score += right * left;
             }
