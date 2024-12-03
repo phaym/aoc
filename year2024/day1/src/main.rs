1938f35b044f157f83bc9dc0febd084d8b41e3e0
phaym <paul.haym@gmail.com>
2024-12-03 00:12:06 -0500

    reorg rust by year

--- year2024/day1/src/main.rs
+++
@@ -1,6 +0,0 @@
-pub mod day1;
-
-fn main() {
-    day1::part1::run("./input.txt");
-    day1::part2::run("./input.txt");
-}
---
+++ year2024/src/main.rs
@@ -0,0 +1,7 @@
+pub mod day1;
+pub mod day2;
+
+fn main() {
+    // day1::part1::run("./input.txt");
+    // day1::part2::run("./input.txt");
+}
