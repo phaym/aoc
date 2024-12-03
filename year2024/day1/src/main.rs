aecc4a51a294ed8297e65aa511abfbc4db6edf47
phaym <paul.haym@gmail.com>
2024-12-02 22:39:20 -0500

    move part1 to a module

--- year2024/day1/src/main.rs
+++ year2024/day1/src/main.rs
@@ -1,32 +1,5 @@
-use std::fs;
+mod part1;
 
 fn main() {
-    let file_contents = fs::read_to_string("./input.txt").expect("invalid input file");
-    let (left, right) = parse_id_lists(file_contents);
-    let distance = calculate_distance(left, right);
-    println!("distance: {}", distance);
-}
-
-fn calculate_distance(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
-    left.sort();
-    right.sort();
-    let mut distance = 0;
-    for i in 0..left.len() {
-        distance += (left[i] - right[i]).abs();
-    }
-    distance
-}
-
-fn parse_id_lists(file_contents: String) -> (Vec<i32>, Vec<i32>) {
-    let mut left: Vec<i32> = Vec::new();
-    let mut right: Vec<i32> = Vec::new();
-    for line in file_contents.lines() {
-        let ids: Vec<i32> = line
-            .split_whitespace()
-            .map(|id| id.parse::<i32>().expect("invalid formatting for line"))
-            .collect();
-        left.push(ids[0]);
-        right.push(ids[1]);
-    }
-    (left, right)
+    part1::run("./input.txt");
 }
