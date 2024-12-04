0ed35ddd432002a9fd86b947a723dfb7ed26e98c
phaym <paul.haym@gmail.com>
2024-12-03 19:44:42 -0500

    refactor to simplify

--- 2024/src/day2.rs
+++ 2024/src/day2.rs
@@ -1,16 +1,5 @@
 use std::fs;
 
-#[derive(Debug)]
-struct Report {
-    levels: Vec<i32>,
-}
-
-impl Report {
-    fn new() -> Self {
-        Report { levels: Vec::new() }
-    }
-}
-
 fn parse_line(line: &str) -> Vec<i32> {
     let ids: Vec<i32> = line
         .split_whitespace()
@@ -19,13 +8,12 @@ fn parse_line(line: &str) -> Vec<i32> {
     ids
 }
 
-fn parse_file(file_path: &str) -> Vec<Report> {
+fn parse_file(file_path: &str) -> Vec<Vec<i32>> {
     let file_contents = fs::read_to_string(file_path).expect("invalid input file");
-    let mut reports: Vec<Report> = Vec::new();
+    let mut reports = Vec::new();
     for line in file_contents.lines() {
-        let mut report = Report::new();
-        report.levels = parse_line(line);
-        reports.push(report);
+        let levels = parse_line(line);
+        reports.push(levels);
     }
     reports
 }
@@ -37,29 +25,30 @@ pub mod part1 {
         println!("running day2 part1");
         let reports = parse_file(file_path);
         let mut safe_count = 0;
-        for level in reports.iter().map(|report| &report.levels) {
-            let mut diff: i32 = 0;
-            let mut safe = true;
-            for i in 1..level.len() {
-                let current_diff = level[i] - level[i - 1];
-                if current_diff.abs() > 3 || current_diff.abs() < 1 {
-                    println!("unsafe diff of {} at i:{}", current_diff, i);
-                    safe = false;
-                    break;
-                }
-                if current_diff < 0 && diff > 0 || current_diff > 0 && diff < 0 {
-                    println!("changed direction at i:{}", i);
-                    safe = false;
-                    break;
-                }
-                diff = current_diff;
-            }
+        for level in reports.iter() {
+            let safe = check_if_safe(level);
             if safe {
                 safe_count += 1;
             }
         }
         println!("safe count: {}", safe_count);
         safe_count
+    }
+
+    pub fn check_if_safe(level: &Vec<i32>) -> bool {
+        let mut diff: i32 = 0;
+        for i in 1..level.len() {
+            let current_diff = level[i] - level[i - 1];
+            if current_diff.abs() > 3 || current_diff.abs() < 1 {
+                println!("unsafe diff of {} at i:{}", current_diff, i);
+                return false;
+            } else if current_diff < 0 && diff > 0 || current_diff > 0 && diff < 0 {
+                println!("changed direction at i:{}", i);
+                return false;
+            }
+            diff = current_diff;
+        }
+        return true;
     }
 }
 
