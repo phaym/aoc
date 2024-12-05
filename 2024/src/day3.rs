bf5fceded55856861e03227dd7ed44fea9c9c6a6
phaym <paul.haym@gmail.com>
2024-12-04 21:56:36 -0500

    more refactoring

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -5,10 +5,10 @@ pub mod part1 {
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day 3 part1");
-        let mut total = 0;
-        for (num1, num2) in parse_file(file_path) {
-            total += num1 * num2;
-        }
+        let total = parse_file(file_path)
+            .into_iter()
+            .map(|(num1, num2)| num1 * num2)
+            .sum();
         println!("total is: {}", total);
         total
     }
@@ -42,46 +42,44 @@ pub mod part2 {
     pub fn parse_file(file_path: &str) -> Vec<(i32, i32)> {
         let file = fs::File::open(file_path).unwrap();
         let buf = BufReader::new(file);
-        let mut nums_to_multiply: Vec<(i32, i32)> = Vec::new();
         let mut enabled = true;
 
-        for line in buf.lines().map(|l| l.unwrap()) {
-            let (result, is_enabled) = parse_line(&line, enabled);
-            enabled = is_enabled;
-            nums_to_multiply.extend(result);
-        }
-
-        nums_to_multiply
+        buf.lines()
+            .filter_map(Result::ok)
+            .flat_map(|line| {
+                let (result, new_enabled) = parse_line(&line, enabled);
+                enabled = new_enabled;
+                result
+            })
+            .collect()
     }
 
-    pub fn parse_line(line: &str, mut enabled: bool) -> (Vec<(i32, i32)>, bool) {
-        let mut nums_to_multiply: Vec<(i32, i32)> = Vec::new();
+    pub fn parse_line(line: &str, enabled: bool) -> (Vec<(i32, i32)>, bool) {
+        let mut nums_to_multiply = Vec::new();
         let re = Regex::new(r"mul\((\d+),(\d+)\)|don't\(\)|do\(\)").unwrap();
+        let mut current_enabled = enabled;
 
         for cap in re.captures_iter(line) {
             match cap.get(0).unwrap().as_str() {
-                "don't()" => enabled = false,
-                "do()" => enabled = true,
-                _ => {
-                    if enabled {
-                        let num1 = cap.get(1).unwrap().as_str();
-                        let num2 = cap.get(2).unwrap().as_str();
-                        let values = (num1.parse::<i32>().unwrap(), num2.parse::<i32>().unwrap());
-
-                        nums_to_multiply.push(values);
-                    }
+                "don't()" => current_enabled = false,
+                "do()" => current_enabled = true,
+                _ if current_enabled => {
+                    let num1 = cap[1].parse().unwrap();
+                    let num2 = cap[2].parse().unwrap();
+                    nums_to_multiply.push((num1, num2));
                 }
+                _ => {}
             };
         }
-        (nums_to_multiply, enabled)
+        (nums_to_multiply, current_enabled)
     }
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day3 part 2");
-        let mut total = 0;
-        for (num1, num2) in parse_file(file_path) {
-            total += num1 * num2;
-        }
+        let total = parse_file(file_path)
+            .into_iter()
+            .map(|(num1, num2)| num1 * num2)
+            .sum();
         println!("total is: {}", total);
         total
     }
