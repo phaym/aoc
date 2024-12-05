8555b0e5f4f7775883b12ee40b28e1552b90ec65
phaym <paul.haym@gmail.com>
2024-12-04 22:13:52 -0500

    remove nested array

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -5,29 +5,24 @@ pub mod part1 {
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day 3 part1");
-        let total = parse_file(file_path)
-            .into_iter()
-            .map(|(num1, num2)| num1 * num2)
-            .sum();
+        let total = parse_file(file_path).into_iter().sum();
         println!("total is: {}", total);
         total
     }
 
-    pub fn parse_file(file_path: &str) -> Vec<(i32, i32)> {
+    pub fn parse_file(file_path: &str) -> Vec<i32> {
         let file = fs::File::open(file_path).unwrap();
         let buf = BufReader::new(file);
-        let mut nums_to_multiply: Vec<(i32, i32)> = Vec::new();
+        let mut sums = Vec::new();
 
         for line in buf.lines().map(|l| l.unwrap()) {
             let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
             for (_, [num1, num2]) in re.captures_iter(&line).map(|c| c.extract()) {
-                let values = (num1.parse::<i32>().unwrap(), num2.parse::<i32>().unwrap());
-                nums_to_multiply.push(values);
+                sums.push(num1.parse::<i32>().unwrap() * num2.parse::<i32>().unwrap());
             }
         }
-
-        nums_to_multiply
+        sums
     }
 }
 
@@ -39,14 +34,14 @@ pub mod part2 {
 
     use regex::Regex;
 
-    pub fn parse_file(file_path: &str) -> Vec<(i32, i32)> {
+    pub fn parse_file(file_path: &str) -> Vec<i32> {
         let file = fs::File::open(file_path).unwrap();
         let buf = BufReader::new(file);
         let mut enabled = true;
 
         buf.lines()
             .filter_map(Result::ok)
-            .flat_map(|line| {
+            .map(|line| {
                 let (result, new_enabled) = parse_line(&line, enabled);
                 enabled = new_enabled;
                 result
@@ -54,32 +49,29 @@ pub mod part2 {
             .collect()
     }
 
-    pub fn parse_line(line: &str, enabled: bool) -> (Vec<(i32, i32)>, bool) {
-        let mut nums_to_multiply = Vec::new();
+    pub fn parse_line(line: &str, enabled: bool) -> (i32, bool) {
         let re = Regex::new(r"mul\((\d+),(\d+)\)|don't\(\)|do\(\)").unwrap();
         let mut current_enabled = enabled;
+        let mut sum = 0;
 
         for cap in re.captures_iter(line) {
             match cap.get(0).unwrap().as_str() {
                 "don't()" => current_enabled = false,
                 "do()" => current_enabled = true,
                 _ if current_enabled => {
-                    let num1 = cap[1].parse().unwrap();
-                    let num2 = cap[2].parse().unwrap();
-                    nums_to_multiply.push((num1, num2));
+                    let num1: i32 = cap[1].parse().unwrap();
+                    let num2: i32 = cap[2].parse().unwrap();
+                    sum += num1 * num2;
                 }
                 _ => {}
             };
         }
-        (nums_to_multiply, current_enabled)
+        (sum, current_enabled)
     }
 
     pub fn run(file_path: &str) -> i32 {
         println!("running day3 part 2");
-        let total = parse_file(file_path)
-            .into_iter()
-            .map(|(num1, num2)| num1 * num2)
-            .sum();
+        let total = parse_file(file_path).into_iter().sum();
         println!("total is: {}", total);
         total
     }
@@ -107,7 +99,7 @@ pub mod tests {
 
     #[test]
     fn parse_line() {
-        let expected = vec![(2, 4), (8, 5)];
+        let expected = 48;
         let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
         let (result, _) = part2::parse_line(input, true);
         assert_eq!(
