340cc1d433c3b963413f713a2b3fd2b8a765fbc8
phaym <paul.haym@gmail.com>
2024-12-08 20:13:07 -0500

    refactor for part 2

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -1,12 +1,52 @@
+use std::collections::HashMap;
+
+pub fn parse_puzzle(puzzle: &str) -> (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>) {
+    let sections: Vec<&str> = puzzle.split("\n\n").collect();
+    (
+        sections[0]
+            .lines()
+            .map(|line| line.split("|").map(|s| s.parse::<i32>().unwrap()).collect())
+            .fold(HashMap::new(), |mut rules, nums: Vec<i32>| {
+                rules
+                    .entry(nums[0])
+                    .and_modify(|rules| rules.push(nums[1]))
+                    .or_insert(vec![nums[1]]);
+                rules
+            }),
+        sections[1]
+            .lines()
+            .map(|line| line.split(",").map(|s| s.parse::<i32>().unwrap()).collect())
+            .fold(Vec::new(), |mut updates, nums: Vec<i32>| {
+                updates.push(nums);
+                updates
+            }),
+    )
+}
+
+pub fn is_update_valid(rules: &HashMap<i32, Vec<i32>>, updates: &Vec<i32>) -> bool {
+    let mut seen = HashMap::new();
+    for entry in updates {
+        if let Some(must_be_after) = rules.get(entry) {
+            if must_be_after.iter().any(|value| seen.contains_key(value)) {
+                return false;
+            }
+        }
+        seen.entry(entry).or_insert(true);
+    }
+    true
+}
+
 pub mod part1 {
 
-    use std::{collections::HashMap, fs};
+    use std::fs;
+
+    use crate::day5::{is_update_valid, parse_puzzle};
 
     pub fn run(file_path: &str) -> i32 {
         let puzzle = fs::read_to_string(file_path).unwrap();
-        let (rules, updates_list) = parse_puzzle(&puzzle);
+        let (rules, updates) = parse_puzzle(&puzzle);
 
-        let count = updates_list
+        let count = updates
             .iter()
             .filter(|updates| is_update_valid(&rules, updates))
             .map(|updates| updates[updates.len() / 2])
@@ -15,47 +55,35 @@ pub mod part1 {
         println!("count is {}", count);
         return count;
     }
+}
 
-    pub fn is_update_valid(rules: &HashMap<i32, Vec<i32>>, updates: &Vec<i32>) -> bool {
-        let mut seen = HashMap::new();
-        for entry in updates {
-            if let Some(must_be_after) = rules.get(entry) {
-                if must_be_after.iter().any(|value| seen.contains_key(value)) {
-                    return false;
-                }
-            }
-            seen.entry(entry).or_insert(true);
-        }
-        true
+pub mod part2 {
+    use std::fs;
+
+    use crate::day5::{is_update_valid, parse_puzzle};
+
+    pub fn run(file_path: &str) -> i32 {
+        let puzzle = fs::read_to_string(file_path).unwrap();
+        let (rules, updates) = parse_puzzle(&puzzle);
+
+        let count = updates
+            .iter()
+            .filter(|updates| !is_update_valid(&rules, updates))
+            .map(|updates| get_middle_after_fix(updates))
+            .sum();
+
+        println!("count is {}", count);
+        return count;
     }
 
-    pub fn parse_puzzle(puzzle: &str) -> (HashMap<i32, Vec<i32>>, Vec<Vec<i32>>) {
-        let sections: Vec<&str> = puzzle.split("\n\n").collect();
-        (
-            sections[0]
-                .lines()
-                .map(|line| line.split("|").map(|s| s.parse::<i32>().unwrap()).collect())
-                .fold(HashMap::new(), |mut rules, nums: Vec<i32>| {
-                    rules
-                        .entry(nums[0])
-                        .and_modify(|rules| rules.push(nums[1]))
-                        .or_insert(vec![nums[1]]);
-                    rules
-                }),
-            sections[1]
-                .lines()
-                .map(|line| line.split(",").map(|s| s.parse::<i32>().unwrap()).collect())
-                .fold(Vec::new(), |mut updates, nums: Vec<i32>| {
-                    updates.push(nums);
-                    updates
-                }),
-        )
+    pub fn get_middle_after_fix(updates: &Vec<i32>) -> i32 {
+        updates[updates.len() / 2]
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::part1;
+    use super::{parse_puzzle, part1};
 
     #[test]
     pub fn test_parse_puzzle() {
@@ -63,7 +91,7 @@ mod tests {
 12|34
 
 12,34,56,78";
-        let (rules, lines) = part1::parse_puzzle(input);
+        let (rules, lines) = parse_puzzle(input);
         assert_eq!(rules.get(&12).unwrap()[0], 34);
         assert_eq!(lines, vec![vec![12, 34, 56, 78]]);
     }
