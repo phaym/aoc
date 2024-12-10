53430feac7da8f4e7c41dee3bc82c17e3b4e5422
phaym <paul.haym@gmail.com>
2024-12-10 11:39:17 -0500

    more refactoring part 1

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -3,66 +3,52 @@ pub mod part1 {
 
     pub fn run(file_path: &str) -> i32 {
         let map = fs::read_to_string(file_path).unwrap();
-        let count = count_moves(&map);
+        let (matrix, start_pos) = parse_matrix(map);
+        let count = count_moves(matrix, start_pos);
         println!("count is: {}", count);
         count
     }
 
-    #[derive(Debug)]
-    pub struct Coord {
-        row: isize,
-        col: isize,
+    pub fn count_moves(mut matrix: Vec<Vec<char>>, (mut row, mut col): (isize, isize)) -> i32 {
+        let max = matrix.len() as isize;
+
+        let mut count = 0;
+        let deltas: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+        let mut delta_idx = 0;
+        while row < max && row >= 0 && col < max && col >= 0 {
+            if matrix[row as usize][col as usize] != 'X' {
+                count += 1;
+            }
+            matrix[row as usize][col as usize] = 'X';
+
+            let mut delta = deltas[delta_idx];
+            if let Some('#') = try_get_char(&matrix, row + delta.0, col + delta.1) {
+                // change direction
+                delta_idx = (delta_idx + 1) % deltas.len();
+                delta = deltas[delta_idx];
+            }
+            (row, col) = (row + delta.0, col + delta.1);
+        }
+        count
     }
 
-    pub fn parse_matrix(map: &str) -> (Vec<Vec<char>>, Coord) {
-        let mut start = Coord { row: 0, col: 0 };
+    pub fn parse_matrix(map: String) -> (Vec<Vec<char>>, (isize, isize)) {
+        let mut start_pos = (0, 0);
         let matrix = map
             .lines()
             .into_iter()
             .enumerate()
             .map(|(i, line)| {
                 if let Some(j) = line.find('^') {
-                    start = Coord {
-                        row: i as isize,
-                        col: j as isize,
-                    };
+                    start_pos = (i as isize, j as isize);
                 };
                 line.chars().collect()
             })
             .collect::<Vec<Vec<char>>>();
-        (matrix, start)
+        (matrix, start_pos)
     }
 
-    pub fn count_moves(map: &str) -> i32 {
-        let (mut matrix, mut coord) = parse_matrix(map);
-        let max = matrix.len() as isize;
-
-        let mut count = 0;
-        let deltas: [(isize, isize); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
-        let mut delta_idx = 0;
-        while coord.row < max && coord.row >= 0 && coord.col < max && coord.col >= 0 {
-            if matrix[coord.row as usize][coord.col as usize] != 'X' {
-                count += 1;
-            }
-            matrix[coord.row as usize][coord.col as usize] = 'X';
-
-            let mut delta = deltas[delta_idx];
-            let (next_row, next_col) = (coord.row + delta.0, coord.col + delta.1);
-            if let Some('#') = try_get_char(&matrix, next_row, next_col) {
-                delta_idx += 1;
-                if delta_idx >= deltas.len() {
-                    delta_idx = 0;
-                }
-                delta = deltas[delta_idx];
-            }
-
-            coord.row += delta.0;
-            coord.col += delta.1;
-        }
-        count
-    }
-
-    pub fn try_get_char(matrix: &Vec<Vec<char>>, row: isize, col: isize) -> Option<char> {
+    fn try_get_char(matrix: &Vec<Vec<char>>, row: isize, col: isize) -> Option<char> {
         matrix
             .get(row as usize)
             .and_then(|row| row.get(col as usize))
@@ -72,7 +58,7 @@ pub mod part1 {
 
 #[cfg(test)]
 pub mod tests {
-    use crate::day6::part1;
+    use crate::day6::part1::{self, parse_matrix};
 
     #[test]
     pub fn test_file_1() {
@@ -87,7 +73,8 @@ pub mod tests {
 ........#.
 #.........
 ......#...";
-        let count = part1::count_moves(map);
+        let (matrix, start_pos) = parse_matrix(map.to_string());
+        let count = part1::count_moves(matrix, start_pos);
         assert_eq!(count, 41);
     }
 
