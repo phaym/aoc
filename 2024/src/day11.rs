42ef7ad42972ebbc2841716aa3a9161b3f872a5b
paul hayman <paul.haym@gmail.com>
2024-12-20 10:19:11 -0500

    solved part1

--- 2024/src/day11.rs
+++ 2024/src/day11.rs
@@ -7,27 +7,34 @@ pub mod part1 {
         let mut parsed = file
             .trim()
             .split(' ')
-            .map(|c| c.parse::<i32>().expect(&format!("{:?}", c)))
+            .map(|c| c.parse::<u64>().expect(&format!("{:?}", c)))
             .collect();
         for _i in 0..25 {
             blink(&mut parsed);
         }
+        println!("result: {}", parsed.len());
         parsed.len()
     }
 
-    pub fn blink(stones: &mut Vec<i32>) {
-        for i in 0..stones.len() {
+    pub fn blink(stones: &mut Vec<u64>) {
+        let mut i = 0;
+        while i < stones.len() {
             let stone = stones[i];
             let stone_str = stone.to_string();
             if stone == 0 {
-                stones[i] = 1 as i32;
+                stones[i] = 1 as u64;
             } else if stone_str.len() % 2 == 0 {
                 let (first, last) = stone_str.split_at(stone.to_string().len() / 2);
-                stones[i] = first.parse::<i32>().unwrap();
-                stones.insert(i + 1, last.parse::<i32>().unwrap());
+                let (first_int, last_int) = (first.parse().unwrap(), last.parse().unwrap());
+                // println!("str:{stone_str} first:{first}->{first_int}, last:{last}->{last_int}");
+                stones[i] = first_int;
+                stones.insert(i + 1, last_int);
+                i += 1;
             } else {
+                // println!("times {stone}");
                 stones[i] *= 2024;
             }
+            i += 1;
         }
     }
 }
@@ -38,9 +45,14 @@ pub mod tests {
 
     #[test]
     pub fn test_blink() {
-        let mut input = vec![125, 17];
-        blink(&mut input);
-        assert_eq!(input, vec![253000, 1, 7]);
+        let tests = vec![
+            (vec![125, 17], vec![253000, 1, 7]),
+            (vec![253000, 1, 7], vec![253, 0, 2024, 14168]),
+        ];
+        for (mut input, expected) in tests {
+            blink(&mut input);
+            assert_eq!(input, expected);
+        }
     }
 
     #[test]
