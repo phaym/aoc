833f8eda322d2d08c0880e1137b77acf471c2caf
phaym <1431593+phaym@users.noreply.github.com>
2026-02-02 18:29:22 -0500

    solved part 2

--- 2025/src/day2.test.ts
+++ 2025/src/day2.test.ts
@@ -1,9 +1,23 @@
-import { describe, test } from 'vitest';
-import { day2 } from './day2.js';
+import { describe, expect, test } from 'vitest';
+import { day2, isRepeated2 } from './day2.js';
 
 export const FILE_PATH = 'input/day2/example.txt';
+
 describe('day2', () => {
   test('test1', async () => {
     await day2(FILE_PATH);
+  });
+
+  describe('part2', () => {
+    test('repeated', () => {
+      expect(isRepeated2('123123')).toBe(true);
+      expect(isRepeated2('123123123')).toBe(true);
+      expect(isRepeated2('1231231234')).toBe(false);
+      expect(isRepeated2('123123123123123')).toBe(true);
+      expect(isRepeated2('1212121212')).toBe(true);
+    });
+    test('special', () => {
+      expect(isRepeated2('1111111')).toBe(true);
+    });
   });
 });
--- 2025/src/day2.ts
+++ 2025/src/day2.ts
@@ -9,7 +9,7 @@ export const day2 = async (filePath = FILE_PATH) => {
       .trim()
       .split('-')
       .map((val) => +val);
-    const invalid = validateProductRange(range);
+    const invalid = validateProductRange(range, 2);
     allInvalid = [...allInvalid, ...invalid];
   }
   const total = allInvalid.reduce((prev, curr) => {
@@ -18,12 +18,12 @@ export const day2 = async (filePath = FILE_PATH) => {
   console.log(`got total: ${total}`);
 };
 
-const validateProductRange = ([start, end]: number[]): number[] => {
+const validateProductRange = ([start, end]: number[], part = 1): number[] => {
   const invalidInRange: number[] = [];
   console.log(`start: ${start} end: ${end}`);
   for (let i = start; i <= end; i++) {
     const id = i.toString();
-    const invalid = isRepeated(id);
+    const invalid = part === 1 ? isRepeated(id) : isRepeated2(id);
     if (invalid) {
       console.log(`found invalid num: ${id}`);
       invalidInRange.push(+id);
@@ -38,6 +38,31 @@ const isRepeated = (id: string) => {
   for (let l = 0; l < id.length / 2; l++) {
     const r = l + id.length / 2;
     if (id[l] !== id[r]) return false;
+  }
+  return true;
+};
+
+export const isRepeated2 = (id: string) => {
+  for (let repeats = 2; repeats <= id.length; repeats++) {
+    if (id.length % repeats !== 0) continue;
+    if (isRepeatedForGap(id, repeats)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const isRepeatedForGap = (id: string, repeats: number) => {
+  const pointers = [];
+  const gap = id.length / repeats;
+  for (let i = 1; i < repeats; i++) {
+    pointers.push(i * gap);
+  }
+  for (let l = 0; l < gap; l++) {
+    for (const pointer of pointers) {
+      const pos = pointer + l;
+      if (id[l] !== id[pos]) return false;
+    }
   }
   return true;
 };
