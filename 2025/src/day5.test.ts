2aa21d4d28adc18c0af8606296e897a8b3eadd34
phaym <1431593+phaym@users.noreply.github.com>
2026-02-03 15:24:14 -0500

    completed part 2

--- 2025/src/day5.test.ts
+++ 2025/src/day5.test.ts
@@ -1,9 +1,58 @@
 import { describe, expect, test } from 'vitest';
-import { day5 } from './day5.js';
+import { countRanges, day5 } from './day5.js';
 
 const TEST_FILE = 'input/day5/example.txt';
 describe('day5', () => {
   test('test file', async () => {
     expect(await day5(TEST_FILE)).toBe(3);
+  });
+
+  test('part 2', () => {
+    const ranges = [
+      [
+        [10, 15],
+        [10, 15],
+      ],
+      [
+        [10, 13],
+        [10, 15],
+      ],
+      [
+        [10, 15],
+        [10, 13],
+      ],
+      [
+        [10, 15],
+        [12, 15],
+      ],
+      [
+        [12, 15],
+        [10, 15],
+      ],
+      [
+        [10, 15],
+        [12, 13],
+      ],
+      [
+        [12, 13],
+        [10, 15],
+      ],
+      [
+        [10, 13],
+        [12, 15],
+      ],
+      [
+        [12, 15],
+        [10, 13],
+      ],
+      [
+        [12, 15],
+        [10, 13],
+        [11, 12],
+      ],
+    ];
+    for (const range of ranges) {
+      expect(countRanges(range)).toBe(6);
+    }
   });
 });
--- 2025/src/day5.ts
+++ 2025/src/day5.ts
@@ -7,35 +7,54 @@ export async function day5(filePath = FILE_PATH) {
   const rl = readline.createInterface(rs);
   const freshRanges = [];
   const ingredients = [];
-  let reading_ranges = true;
   for await (const line of rl) {
-    if (line === '') {
-      reading_ranges = false;
-    } else if (reading_ranges) {
-      freshRanges.push(line.split('-').map((val) => +val));
+    if (!line.length) continue;
+    const parsedLine = line.split('-').map((val) => +val);
+    if (parsedLine.length > 1) {
+      freshRanges.push(parsedLine);
     } else {
-      ingredients.push(+line);
+      ingredients.push(...parsedLine);
     }
   }
   const freshCount = countFreshIngredients(ingredients, freshRanges);
-  console.log(`total count ${freshCount}`);
+  const part2 = countRanges(freshRanges);
+  console.log(`total count ${freshCount} part2:${part2}`);
   return freshCount;
 }
 
-function countFreshIngredients(ingredients: number[], fresh_ranges: number[][]) {
+export const countRanges = (freshRanges: number[][]) => {
+  const sorted = freshRanges.sort((a, b) => a[0] - b[0]);
+  let start = sorted[0][0];
+  let end = sorted[0][1];
+  let total = 0;
+  for (let i = 1; i < freshRanges.length; i++) {
+    const currStart = sorted[i][0];
+    const currEnd = sorted[i][1];
+    if (currStart <= end) {
+      end = Math.max(currEnd, end);
+    } else {
+      total += end - start + 1;
+      start = currStart;
+      end = currEnd;
+    }
+  }
+  total += end - start + 1;
+  return total;
+};
+
+function countFreshIngredients(ingredients: number[], freshRanges: number[][]) {
   let freshCount = 0;
   for (const ingredient of ingredients) {
-    const isFresh = checkIsFresh(ingredient, fresh_ranges);
+    const isFresh = checkIsFresh(ingredient, freshRanges);
     if (isFresh) freshCount++;
   }
   return freshCount;
 }
 
-function checkIsFresh(ingredient: number, fresh_ranges: number[][]) {
-  for (const range of fresh_ranges) {
+function checkIsFresh(ingredient: number, freshRanges: number[][]) {
+  for (const range of freshRanges) {
     const [start, end] = range;
     if (ingredient >= start && ingredient <= end) {
-      console.log(`ingredient: ${ingredient} start: ${start} end: ${end}`);
       return true;
     }
   }
